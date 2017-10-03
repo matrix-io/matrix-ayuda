7375cdb37ac5def9d507b4cedd266a9f41e562a1
eqsnull <jorgecastilloltsd@gmail.com>
2017-10-03 13:12:33 -0400

    Add mock tests

--- test/ayuda.test.js
+++ test/ayuda.test.js
@@ -1,47 +1,112 @@
 const Ayuda = require('../index.js');
-const sinon = require('sinon');
 const expect = require('chai').expect;
+const nock = require('nock');
 
-
-// () => {}
-// Arrow functions are discouraged in mocha
+/*
+    TODO: add switch that performs test on actual ayuda service
+    TODO: should makeRequest throw errors?
+    TODO: should we add mock functionality into the module itself? (could be useful for app testing)
+ */
 
 describe('Ayuda', function() {
 
     let ayuda;
+    let userInfo;
 
     before(function(){
 
-        ayuda = new Ayuda({
-            username: process.env.AY_API_USER || '',
-            password: process.env.AY_API_PASSWORD || '',
+        userInfo = {
+            username: process.env.AY_API_USER || 'test',
+            password: process.env.AY_API_PASSWORD || 'testpass',
             apiUrl: process.env.AY_API_URL || 'https://pv.ayudapreview.com/Juice/pi'
-        });
+        };
+
+        ayuda = new Ayuda(userInfo);
 
     });
 
+    // makeRequest should handle errors and successful requests appropriately
     describe('#makeRequest()', function(){
 
-        it('should handle 200 successfully');
-        it('should throw error on 302');
-        it('should throw error on ###');
+        it('should handle 200 successfully', function(done){
+
+            let fakeAyuda = nock(userInfo.apiUrl)
+                .get('/')
+                .reply(200, 'path matched!');
+
+            ayuda.makeRequest('GET','/', {},function(error, response, body){
+
+                expect(response.statusCode).to.equal(200);
+                done();
+
+            });
+
+        });
+
+        it('should throw error when response code != 200', function(done){
+
+            let fakeAyuda = nock(userInfo.apiUrl)
+                .get('/')
+                .reply(302, 'found');
+
+            ayuda.makeRequest('GET', '/', {}, function (error, response, body) {
+                expect(error).to.not.be.undefined;
+                done();
+            });
+
+
+        });
 
     });
 
     describe('#login()', function() {
 
         it('login successfully and return sessionID ', function(done) {
+
+            let fakeAyudaLogin = nock(userInfo.apiUrl)
+                .post('/Session/Login')
+                .basicAuth({
+
+                    user: userInfo.username,
+                    pass: userInfo.password
+
+                })
+                .reply(200, {
+
+                    sessionID : '8a3feb73-a869-4e99-8d0e-ca8b33590726'
+
+                });
+
             ayuda.login(function(err, sessionID) {
 
-                expect(err, "recieved error").to.be.null;
-                expect(sessionID, "session").to.be.a('string');
-
-                console.log(sessionID);
+                expect(sessionID).to.be.a('string');
                 done();
 
             });
         });
 
+
+        it('return error on unsuccesful login', function(done){
+
+            let fakeAyudaLogin = nock(userInfo.apiUrl)
+
+                .post('/Session/Login')
+                .basicAuth({
+
+                    user: userInfo.username,
+                    pass: userInfo.password
+
+                })
+                .reply(401, 'Unauthorized');
+
+            ayuda.login(function(err, sessionID) {
+
+                expect(err).to.not.be.undefined;
+                done();
+
+            });
+
+        });
     });
 
     describe('#getDigitalPlayLogs', function(){
@@ -49,10 +114,54 @@ describe('Ayuda', function() {
         // Playlogs are a large and could take a while
         // disable timeouts
         this.timeout(0);
+        let dummyLogs;
+
+        before(function(){
+
+            dummyLogs = [
+
+                {
+                    MediaFileName: "foo",
+                    Times: [
+                        {"DateTime": "2017-08-04T00:00:01"},
+                        {"DateTime": "2017-08-04T00:00:04"},
+                        {"DateTime": "2017-08-04T00:00:07"}
+                    ],
+                },
+                {
+                    MediaFileName: "bar",
+                    Times: [
+                        {"DateTime": "2017-08-04T00:00:02"},
+                        {"DateTime": "2017-08-04T00:00:05"},
+                        {"DateTime": "2017-08-04T00:00:08"}
+                    ],
+                },
+                {
+                    MediaFileName: "baz",
+                    Times: [
+                        {"DateTime": "2017-08-04T00:00:03"},
+                        {"DateTime": "2017-08-04T00:00:06"},
+                        {"DateTime": "2017-08-04T00:00:09"}
+                    ]
+                },
+            ];
+
+        });
 
         it('should obtain PoPs for the day', function(done){
-            ayuda.getDigitalPlayLogs(new Date(), function(err,logs){
 
+            let fakeAyudaGetDigitalPlayLogs = nock(userInfo.apiUrl)
+
+                .post('/Player/GetDigitalPlayLogs')
+                .basicAuth({
+
+                    user: userInfo.username,
+                    pass: userInfo.password
+
+                })
+                .reply(200, dummyLogs);
+
+            ayuda.getDigitalPlayLogs(new Date(), function(err,logs){
 
                 expect(logs, "play logs")
                     .to.be.an('array')
@@ -65,11 +174,6 @@ describe('Ayuda', function() {
 
     });
 
-    describe('#setPlayer()', function(){
-
-        it('should set player id in object');
-
-    });
 
     describe('#flattenPlaylog()', function(){
 
@@ -111,10 +215,29 @@ describe('Ayuda', function() {
 
             // TODO: test for order
             let result = ayuda.flattenPlayLog(dummyLogs);
+
             expect(result).to.be.an('array');
-            console.log(result);
             done();
+
         });
+
+    });
+
+    describe('#getTimeZone()',function(){
+
+        it('should get timezone data from player');
+
+    });
+
+    describe('#setPlayer()', function(){
+
+        it('should set player id in object');
+
+    });
+
+    describe('#setDigitalFaceCode()', function(){
+
+        it('should set digitalfacecode');
 
     });
 });
